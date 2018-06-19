fc9178473bf13617e313ed7fac53a3ea45a0aa22
Teya <dlechuga30@gmail.com>
2018-06-19 14:37:38 -0400

    uploading coding dojo mongodb message board assignment

---
+++ server.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
+
+
+
+app.listen(8000, function() {
+	console.log("server running on port 8000");
+});
+
+mongoose.connect('mongodb://127.0.0.1/message_board', function(err, db) {
+	if (err) {
+		console.log("error here");
+		console.log(err);
+	}
+});
+
+// Use native promises
+mongoose.Promise = global.Promise;
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new mongoose.Schema({
+	name: String,
+	message: String,
+	_comments: [{type: Schema.Types.ObjectId, ref: 'Comment'}]
+},{ timestamps: true });
+
+mongoose.model('Message', MessageSchema);
+const Message = mongoose.model("Message");
+
+
+const CommentSchema = new mongoose.Schema({
+	name: String,
+	text: String,
+	_message: {type: Schema.Types.ObjectId, ref: 'Message'}
+});
+
+
+mongoose.model("Comment", CommentSchema);
+
+const Comment = mongoose.model("Comment");
+
+app.get('/', function(req, res){
+	arr = Message.find({}).populate('_comments').exec(function(err, messages){
+		res.render('index', {m:messages})
+	});
+});
+
+
+app.post("/message", function(req, res){
+	var newMessage = new Message({ name: req.body.name, message: req.body.message });
+	newMessage.save(function(err) {
+		if (err) {
+			console.log(err);
+			res.render('index.ejs', { errors: newMessage.errors });
+		} else {
+			console.log("success");
+			res.redirect('/');
+		}
+	})
+})
+
+app.post("/comment/:id", function(req, res) {
+	const messageId = req.params.id;
+	Message.findOne({ _id: messageId }, function(err, message) {
+		const newComment = new Comment({ name: req.body.name, text: req.body.comment });
+		newComment._message = message._id;
+		Message.update({ _id: message._id }, { $push: { _comments: newComment }}, function(err) {
+
+		});
+		newComment.save(function(err) {
+			if (err) {
+				console.log(err);
+				res.render('index.ejs', { errors: newComment.errors });
+			} else {
+				console.log("comment added");
+				res.redirect("/");
+			}
+		});
+	});
+});
